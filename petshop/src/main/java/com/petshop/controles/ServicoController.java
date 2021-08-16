e8852d19df77039f349f1e0d4ab2bfa44c550693
michaelfarias <michaelfarias01@gmail.com>
2021-08-16 15:12:14 -0300

    cadastrando servico pelo controller

--- petshop/src/main/java/com/petshop/controles/ServicoController.java
+++ petshop/src/main/java/com/petshop/controles/ServicoController.java
@@ -1,12 +1,18 @@
 package com.petshop.controles;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.petshop.modelo.Servico;
 import com.petshop.servicos.ServicoService;
 
 @RestController
@@ -24,5 +30,14 @@ public class ServicoController {
 	@GetMapping(value = "nome")
 	public ResponseEntity<?> listaServicoPorNome(@RequestParam(value = "nome") String nome) {
 		return ResponseEntity.ok().body(service.findDistinctByNomeContaining(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<?> cadastrarServico(@RequestBody Servico servico) {
+		Servico obj = service.insert(servico);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
--- petshop/src/main/java/com/petshop/servicos/ServicoService.java
+++ petshop/src/main/java/com/petshop/servicos/ServicoService.java
@@ -23,4 +23,8 @@ public class ServicoService {
 		return repo.findDistinctByNomeContaining(nome);
 	}
 
+	public Servico insert(Servico servico) {
+		return repo.save(servico);
+	}
+
 }
