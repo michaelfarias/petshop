6920d63366507ed9454a05023b01a67de1db9e5c
michaelfarias <michaelfarias01@gmail.com>
2021-09-08 17:07:58 -0300

    Iniciando validação de campos no frontend com formik

--- frontend/src/pages/CadastrarCliente.tsx
+++ frontend/src/pages/CadastrarCliente.tsx
@@ -1,40 +1,91 @@
 import { FormEvent, ChangeEvent, useState } from "react";
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 import api from '../services/api';
+
+const sigupSchema = yup.object().shape({
+  nome: yup.string().required('Campo obrigatório'),
+  email: yup.string().email('Email inválido').required('Campo obrigatório'),
+  cpf: yup.string().required('Campo obrigatório'),
+  telefone: yup.string().required('Campo obrigatório'),
+  login: yup.string().required('Campo obrigatório'),
+  senha: yup.string().required('Campo obrigatório')
+})
 
 export function CadastrarCliente() {
 
-    const [values, setValues] = useState({
-        nome: '',
-        email: '',
-        cpf: '',
-        telefone: '',
-        login: '',
-        senha: ''
-    })
+  const [values, setValues] = useState({
+    nome: '',
+    email: '',
+    cpf: '',
+    telefone: '',
+    login: '',
+    senha: ''
+  })
 
-    function handleCadastrarCliente(event: FormEvent) {
-        event.preventDefault();
-        console.log(values)
-        api.post('clientes', values).then(res => { }).catch(erro => {
-            const { data } = erro.response
-            console.log(data)
+  function handleCadastrarCliente(event: FormEvent) {
+    event.preventDefault();
+    console.log(values)
+    api.post('clientes', values).then(res => { }).catch(erro => {
+      const { data } = erro.response
+      console.log(data)
 
-        });
+    });
 
-    }
+  }
 
-    function handleSetState(event: ChangeEvent<HTMLInputElement>) {
-        const name = event.target.name as string
-        const newValue = event.target.value;
+  function handleSetState(event: ChangeEvent<HTMLInputElement>) {
+    const name = event.target.name as string
+    const newValue = event.target.value;
 
-        setValues({ ...values, [name]: newValue });
-    }
+    setValues({ ...values, [name]: newValue });
+  }
 
-    return (
-        <div>
-            <h1>Cadastro de Cliente</h1>
-            <form onSubmit={handleCadastrarCliente}>
+  return (
+    <div>
+      <h1>Cadastro de Cliente</h1>
+
+      <Formik
+        initialValues={{
+          nome: '',
+          email: '',
+          cpf: '',
+          telefone: '',
+          login: '',
+          senha: ''
+        }}
+        validationSchema={sigupSchema}
+        onSubmit={values => {
+          console.log(values)
+        }}
+      >
+        {
+          () => (
+            <Form>
+              Nome: <Field name="nome" />
+              <ErrorMessage name='nome' /><br />
+
+              Email: <Field name="email" />
+              <ErrorMessage name='email' /><br />
+
+              Cpf:<Field name="cpf" />
+              <ErrorMessage name="cpf" /><br />
+
+              Telefone:<Field name="telefone" />
+              <ErrorMessage name="telefone" /><br />
+
+              Login:<Field name="login" />
+              <ErrorMessage name="login" /><br />
+
+              Senha:<Field name="senha" type="password" />
+              <ErrorMessage name="senha" /><br />
+
+              <button type='submit'>Cadastrar</button>
+            </Form>
+          )
+        }
+      </Formik>
+      {/* <form onSubmit={handleCadastrarCliente}>
                 Nome:  <input type="text" placeholder="Nome do cliente" name="nome" onChange={event => { setValues({ ...values, [event.target.name]: event.target.value }) }} /><br />
                 Email: <input type="email" name="email" onChange={handleSetState} /><br />
                 Cpf: <input type="text" name="cpf" onChange={handleSetState} /><br />
@@ -44,7 +95,7 @@ export function CadastrarCliente() {
                 <button type="submit">
                     Cadastrar
                 </button>
-            </form>
-        </div>
-    );
+            </form> */}
+    </div>
+  );
 }
